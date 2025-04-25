bda8f0c6e25d15c20f9a1c5711a5776a01bcae17
Meradji Anaïs <126171111+meradji-anais@users.noreply.github.com>
2025-04-25 23:57:46 +0100

    Update 1_create_database.sql

--- 1_create_database.sql
+++ 1_create_database.sql
@@ -1,5 +1,7 @@
 -- Create LabDatabase Database
 
+CREATE DATABASE LabDatabase; 
+
 GO
 
 -- Use LabDatabase Database
@@ -7,9 +9,20 @@ USE LabDatabase;
 GO 
 
 -- Create Laboratoire Table
-
+CREATE TABLE Laboratoire ( 
+  CodeLab varchar(4) primary key,
+  NomLab varchar(100),
+  Datcreation Date,
+  DatFin Date, 
+  Siteweb NVARCHAR(255) );
 
 -- Create Chercheur Table 
-
+CREATE TABLE Chercheur (
+  NumCh integer primary key, 
+  Nomch varchar(30),
+  CodeLab varchar(4),
+  foreign key (CodeLab) references Laboratoire(CodeLab));
+  
+  
 
 GO

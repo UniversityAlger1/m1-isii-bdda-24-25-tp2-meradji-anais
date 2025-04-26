4fde7871f8deaf0596acfbbbfc654ca496472c10
Meradji Anaïs <126171111+meradji-anais@users.noreply.github.com>
2025-04-26 14:53:19 +0100

    Update 3_3_allChercheurs.sql

--- 3_3_allChercheurs.sql
+++ 3_3_allChercheurs.sql
@@ -1,1 +1,7 @@
+select chercheur.NumCh, chercheur.Nomch from laboratoire join chercheur on chercheur.CodeLab=laboratoire.CodeLab where laboratoire.CodeLab=0001;
 
+--ps : je ne suis pas obligé d'utiliser un join mais bon je l'ai fait de cette facon là quand meme
+
+une autre requete qui est correcte également: 
+
+select NumCh, Nomch from chercheur where CodeLab=1;
